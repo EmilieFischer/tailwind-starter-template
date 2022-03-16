71323242518c6c8ad0623dd87022331543b3ef18
Emilie Fischer <emil609u@stud.kea.dk>
2022-03-16 22:48:59 +0100

    arbejde

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,26 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-  },
+    fontFamily: {
+      'header-font': ['"Anton"', 'sans-serif'],
+    },
+    colors: {
+      'bg-color': '#ECEADF',
+      'bg-white-box': '#ffffff',
+      'text-black': '#000000',
+      'text-hover': '#ffffff',
+
+
+      'box1': '#EDE6C5',
+      'box2': '#D6CCA0',
+    },
+      extend: {
+        backgroundImage: {
+          'box-background': "url('./img/img.png')",
+          'bg-img': "url('./img/bg_image.jpg')",
+
+      }
+    },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
+  }
 };
